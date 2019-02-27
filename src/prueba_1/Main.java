d46fb478694eed423a87b136bc64cf62fcd45161
Lenovo-PC\USUARIO <eramos933@gmail.com>
2019-02-27 13:08:59 +0100

    Initial commit

---
+++ src/prueba_1/Main.java
@@ -0,0 +1,26 @@
+
+package prueba_1;
+import java.lang.Math;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		double y = 0.5;
+		System.out.println(y);
+		System.out.println(y);
+		double x = Math.sqrt(y);
+		System.out.println(x);
+		
+		if(x < y)
+			System.out.println("x es menor que y");
+		else if (x == y)
+			System.out.println("x es igual que y");
+		else
+			System.out.println("x es mayor que y");
+		
+		switch()
+
+	}
+
+}
